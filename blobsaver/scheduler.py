43234fe08432f5326d12deeaf52ca1f1381b10f4
dedins <andrea.dedominicis.93@gmail.com>
2023-02-08 21:50:20 +0100

    Update scheduler.py

--- blobsaver/scheduler.py
+++ blobsaver/scheduler.py
@@ -5,11 +5,11 @@ from datetime import datetime
 import subprocess
 
 def job():
-print('\033[92m'+datetime.now().strftime("[%H:%M:%S]")+" INFO: Starting blobsaver...")
-cmd = subprocess.run(["/app/bin/blobsaver", "--background-autosave"], capture_output=True)
-stdout = str(cmd.stdout).replace('\\n', '\n')
-print('\033[94m'+datetime.now().strftime("[%H:%M:%S]")+" INFO: Output:")
-print(stdout)
+    print('\033[92m'+datetime.now().strftime("[%H:%M:%S]")+" INFO: Starting blobsaver...")
+    cmd = subprocess.run(["/app/bin/blobsaver", "--background-autosave"], capture_output=True)
+    stdout = str(cmd.stdout).replace('\\n', '\n')
+    print('\033[94m'+datetime.now().strftime("[%H:%M:%S]")+" INFO: Output:")
+    print(stdout)
     
 schedule.every().day.at("10:30").do(job)
 
