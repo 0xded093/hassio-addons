e8ab0dbaca21a11ddb2d031058fdb03421c53790
dedins <andrea.dedominicis.93@gmail.com>
2023-06-14 15:31:20 +0200

    Update start.sh

--- stremio/start.sh
+++ stremio/start.sh
@@ -2,15 +2,15 @@
 
 bashio::log.info "Patching stremio server.js ..."
 
-# bashio::log.info "  1) Forcing HTTPS for serverURL"
-# sed -i "s~var serverUrl = encodeURIComponent( protocol + req.headers.host);~var serverUrl = encodeURIComponent( 'https://' + req.headers.host);~g" server.js
+bashio::log.info "  1) Forcing HTTPS for serverURL"
+sed -i "s~var serverUrl = encodeURIComponent( protocol + req.headers.host);~var serverUrl = encodeURIComponent( 'https://' + req.headers.host);~g" server.js
 
-# bashio::log.info "  2) Fixing unsupported media issues"
-# sed -i -E "s|(var first = req\.params\.first)|if (req.params.first === 'hlsv2') { req.params.first = req.query.mediaURL.split('/')[3]; req.params.second = req.query.mediaURL.split('/')[4]; }\n    \1|" server.js
-# sed -i -E 's/HLS.masterMultiPlaylistMiddleware\)/HLS.masterPlaylistMiddleware\)/' server.js
+bashio::log.info "  2) Fixing unsupported media issues"
+sed -i -E "s|(var first = req\.params\.first)|if (req.params.first === 'hlsv2') { req.params.first = req.query.mediaURL.split('/')[3]; req.params.second = req.query.mediaURL.split('/')[4]; }\n    \1|" server.js
+sed -i -E 's/HLS.masterMultiPlaylistMiddleware\)/HLS.masterPlaylistMiddleware\)/' server.js
 
-# bashio::log.info "  3) Fixing CORS"
-# sed -i 's/if (ok) enginefs.sendCORSHeaders/if (true) enginefs.sendCORSHeaders/g' server.js
+bashio::log.info "  3) Fixing CORS"
+sed -i 's/if (ok) enginefs.sendCORSHeaders/if (true) enginefs.sendCORSHeaders/g' server.js
 
 bashio::log.info "Starting stremio server.js ...)"
 node server.js
