3a61124001384c618c59e5c95687e5c312091c59
dedins <andrea.dedominicis.93@gmail.com>
2023-08-15 20:36:47 +0200

    Update start.sh

--- anisette/start.sh
+++ anisette/start.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bashio
 
-bashio::log.info "Loading configuration..."
+echo "Loading configuration..."
 
-lib_path=$(bashio::config "lib_path")
-
-bashio::log.info "Importing libs from '/share/anisette/lib' ..."
+echo "Importing libs from '/share/anisette/lib' ..."
 cp -R /share/anisette/lib /opt/omnisette-server/lib
 
-bashio::log.info "Libs loaded: $(ls /opt/omnisette-server/lib/*/*)"
+echo "Libs loaded: $(ls /opt/omnisette-server/lib/*/*)"
 
 /opt/omnisette-server/omnisette-server 
